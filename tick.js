e2e99505e229a1ecc387847cd0b0192f1969d06c
bhattaraijyoti <83012733+bhattaraijyoti@users.noreply.github.com>
2024-12-07 19:55:35 +0545

    Add files via upload

---
+++ tick.js
@@ -0,0 +1,20 @@
+function input(){
+  document.getElementById("value")= "x";
+}
+
+
+function display() {
+  const elem = document.getElementById("1");
+  elem.innerHTML = "x"
+
+
+for (let i=1 ; i<10 ; i++){
+  input == "x" ;
+}
+}
+
+  
+  
+  
+
+
